c4cea72e8a95d962d930960a936bd5ef2906f274
duaneyoung <iandologiovanni1@gmail.com>
2017-11-16 12:37:53 +0100

    first commit

---
+++ Miracle Pills/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Miracle Pills
+//
+//  Created by Giovanni Iandolo on 15/11/2017.
+//  Copyright © 2017 Giovanni Iandolo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.white
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
